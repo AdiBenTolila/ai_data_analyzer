cd6e6e95f2d2c52eb8e8e2a89a108e903c27641a
AdiBenTolila <107065716+AdiBenTolila@users.noreply.github.com>
2025-05-18 10:07:19 +0000

    return

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from streamlit_tags import st_tags
 import altair as alt
 from models import first_classification_ai, classify_data, first_classification_ai_gpt, classify_data_gpt
-import re
 
 st.markdown("""
     <style>
@@ -22,14 +21,8 @@ uploaded_file = st.file_uploader("Upload your Excel or CSV file", type=["xlsx", 
 df = None
 selected_sheet = None
 
-def clean_string(input_string):
-      return re.sub(r'[^a-zA-Z0-9_\-\.א-ת]', '_', input_string)
-
 # If a file is uploaded
 if uploaded_file:
-
-    cleaned_filename = clean_string(uploaded_file.name)  # Clean the uploaded file name
-    st.session_state["last_uploaded_file"] = cleaned_filename  # Store cleaned filename
 
     if uploaded_file.name.endswith('.csv'):
         selected_sheet = "CSV file"
